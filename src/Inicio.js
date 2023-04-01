c1a466e98d1fd2136108e16d2d9f8886a65b6d09
dstagno <dstagno@falabella.cl>
2023-04-01 07:18:19 -0300

    update

--- src/Inicio.js
+++ src/Inicio.js
@@ -11,24 +11,132 @@ class Inicio extends React.Component {
           allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
           allowFullScreen
         ></iframe>
-        <iframe class="embed-responsive-item" src="https://www.youtube-nocookie.com/embed/g7PJlf543Ew" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
-        <iframe class="embed-responsive-item" src="https://www.youtube-nocookie.com/embed/tM8LmzKca3k" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
-        <iframe class="embed-responsive-item" src="https://www.youtube-nocookie.com/embed/TB2piLyPNk8" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
-        <iframe class="embed-responsive-item" src="https://www.youtube-nocookie.com/embed/hrwk0COfggo" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
-        <iframe class="embed-responsive-item" src="https://www.youtube-nocookie.com/embed/U75g2mDTXtA" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
-        <iframe class="embed-responsive-item" src="https://www.youtube-nocookie.com/embed/vxtONnys53s" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
-        <iframe class="embed-responsive-item" src="https://www.youtube-nocookie.com/embed/cDTm7Hx26zI" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
-        <iframe class="embed-responsive-item" src="https://www.youtube-nocookie.com/embed/m0SkBGUUGy8" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
-        <iframe class="embed-responsive-item" src="https://www.youtube-nocookie.com/embed/smQC_iVgGEw" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
-        <iframe class="embed-responsive-item" src="https://www.youtube-nocookie.com/embed/aolor5zx3vE" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
-        <iframe class="embed-responsive-item" src="https://www.youtube-nocookie.com/embed/w08UZsqyyr8" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
-        <iframe class="embed-responsive-item" src="https://www.youtube-nocookie.com/embed/8KC-PLSkQn0" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
-        <iframe class="embed-responsive-item" src="https://www.youtube-nocookie.com/embed/uHFJ9qhR0VM" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
-        <iframe class="embed-responsive-item" src="https://www.youtube-nocookie.com/embed/mrojrDCI02k" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
-        <iframe class="embed-responsive-item" src="https://www.youtube-nocookie.com/embed/53N99Nim6WE" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
-        <iframe class="embed-responsive-item" src="https://www.youtube-nocookie.com/embed/P7YMI39sObY" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
-        <iframe class="embed-responsive-item" src="https://www.youtube-nocookie.com/embed/4QA30qkRYy8" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
-        <iframe class="embed-responsive-item" src="https://www.youtube-nocookie.com/embed/TuMfRI2dStE" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+        <iframe 
+          title="YouTube video player"
+          className="embed-responsive-item"
+          src="https://www.youtube-nocookie.com/embed/g7PJlf543Ew"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+          allowFullScreen
+        ></iframe>
+        <iframe 
+          title="YouTube video player"
+          className="embed-responsive-item"
+          src="https://www.youtube-nocookie.com/embed/tM8LmzKca3k"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+          allowFullScreen
+        ></iframe>
+        <iframe 
+          title="YouTube video player"
+          className="embed-responsive-item"
+          src="https://www.youtube-nocookie.com/embed/TB2piLyPNk8"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+          allowFullScreen
+        ></iframe>
+        <iframe 
+          title="YouTube video player"
+          className="embed-responsive-item"
+          src="https://www.youtube-nocookie.com/embed/hrwk0COfggo"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+          allowFullScreen
+        ></iframe>
+        <iframe 
+          title="YouTube video player"
+          className="embed-responsive-item"
+          src="https://www.youtube-nocookie.com/embed/U75g2mDTXtA"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+          allowFullScreen
+        ></iframe>
+        <iframe 
+          title="YouTube video player"
+          className="embed-responsive-item"
+          src="https://www.youtube-nocookie.com/embed/vxtONnys53s"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+          allowFullScreen
+        ></iframe>
+        <iframe 
+          title="YouTube video player"
+          className="embed-responsive-item"
+          src="https://www.youtube-nocookie.com/embed/cDTm7Hx26zI"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+          allowFullScreen
+        ></iframe>
+        <iframe 
+          title="YouTube video player"
+          className="embed-responsive-item"
+          src="https://www.youtube-nocookie.com/embed/m0SkBGUUGy8"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+          allowFullScreen
+        ></iframe>
+        <iframe 
+          title="YouTube video player"
+          className="embed-responsive-item"
+          src="https://www.youtube-nocookie.com/embed/smQC_iVgGEw"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+          allowFullScreen
+        ></iframe>
+        <iframe 
+          title="YouTube video player"
+          className="embed-responsive-item"
+          src="https://www.youtube-nocookie.com/embed/aolor5zx3vE"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+          allowFullScreen
+        ></iframe>
+        <iframe 
+          title="YouTube video player"
+          className="embed-responsive-item"
+          src="https://www.youtube-nocookie.com/embed/w08UZsqyyr8"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+          allowFullScreen
+        ></iframe>
+        <iframe 
+          title="YouTube video player"
+          className="embed-responsive-item"
+          src="https://www.youtube-nocookie.com/embed/8KC-PLSkQn0"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+          allowFullScreen
+        ></iframe>
+        <iframe 
+          title="YouTube video player"
+          className="embed-responsive-item"
+          src="https://www.youtube-nocookie.com/embed/uHFJ9qhR0VM"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+          allowFullScreen
+        ></iframe>
+        <iframe 
+          title="YouTube video player"
+          className="embed-responsive-item"
+          src="https://www.youtube-nocookie.com/embed/mrojrDCI02k"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+          allowFullScreen
+        ></iframe>
+        <iframe 
+          title="YouTube video player"
+          className="embed-responsive-item"
+          src="https://www.youtube-nocookie.com/embed/53N99Nim6WE"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+          allowFullScreen
+        ></iframe>
+        <iframe 
+          title="YouTube video player"
+          className="embed-responsive-item"
+          src="https://www.youtube-nocookie.com/embed/P7YMI39sObY"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+          allowFullScreen
+        ></iframe>
+        <iframe 
+          title="YouTube video player"
+          className="embed-responsive-item"
+          src="https://www.youtube-nocookie.com/embed/4QA30qkRYy8"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+          allowFullScreen
+        ></iframe>
+        <iframe 
+          title="YouTube video player"
+          className="embed-responsive-item"
+          src="https://www.youtube-nocookie.com/embed/TuMfRI2dStE"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+          allowFullScreen
+        ></iframe>
       </div>
     );
   }
