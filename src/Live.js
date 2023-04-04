b71426f9a5f2d2de3f0daceabeb164f75fb79a68
dstagno <dstagno@falabella.cl>
2023-04-04 03:01:18 -0400

    update

--- src/Live.js
+++ src/Live.js
@@ -14,7 +14,7 @@ class Live extends React.Component {
         <iframe 
           title="YouTube video player"
           className="embed-responsive-item"
-          src="https://www.youtube-nocookie.com/embed/ueaANdi5em4"
+          src="https://www.youtube-nocookie.com/embed/iUaevnP1LLg"
           allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
           allowFullScreen
         ></iframe>
