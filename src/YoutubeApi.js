1ea77fece2c422b4d33b18976b3897a31c7ceef3
dinostagno <dpaolostagno@gmail.com>
2023-11-16 04:07:16 -0300

    update

--- src/YoutubeApi.js
+++ src/YoutubeApi.js
@@ -1,45 +1,66 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import axios from 'axios';
+import { Form, FormControl, Button } from 'react-bootstrap';
 
 const YoutubeSearch = () => {
+  const [searchTerm, setSearchTerm] = useState('');
   const [videos, setVideos] = useState([]);
-  const [searchQuery, setSearchQuery] = useState('');
 
-  useEffect(() => {
-    const fetchData = async () => {
-      const response = await axios.get(
-        `https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=10&q=${searchQuery}&type=video&key=YOUR_API_KEY_HERE`
-      );
+  const handleSearch = async () => {
+    try {
+      const response = await axios.get('https://www.googleapis.com/youtube/v3/search', {
+        params: {
+          part: 'snippet',
+          maxResults: 10,
+          key: 'AIzaSyAAwMUdTMI_P3PL5sCs_rmo-wVvvOtJjl8',
+          q: searchTerm
+        }
+      });
+
       setVideos(response.data.items);
-    };
-    fetchData();
-  }, [searchQuery]);
+    } catch (error) {
+      console.error('Error fetching data: ', error);
+    }
+  };
 
-  const handleInputChange = (event) => {
-    setSearchQuery(event.target.value);
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    handleSearch();
   };
 
   return (
-    <div>
-      <h1>YouTube Search</h1>
-      <input type="text" value={searchQuery} onChange={handleInputChange} />
-      <ul>
+    <div className="container mt-4">
+      <Form onSubmit={handleSubmit} className="d-flex">
+        <FormControl
+          type="text"
+          value={searchTerm}
+          onChange={(e) => setSearchTerm(e.target.value)}
+          placeholder="Buscar videos en YouTube"
+          style={{ fontSize: '16px', marginRight: '5px', marginBottom: '15px', flex: '1', outline: 'none', marginTop: '5px' }}
+          className="form-control"
+        />
+        <Button type="submit" style={{ fontSize: '16px', marginLeft: '5px' }} className="btn btn-primary">
+          Buscar
+        </Button>
+      </Form>
+
+      <div style={{ width: '100%', display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
         {videos.map((video) => (
-          <li key={video.id.videoId}>
-            <h2>{video.snippet.title}</h2>
+          <div key={video.id.videoId} style={{ width: '100%', maxWidth: '1600px', marginBottom: '20px' }}>
+            <h3>{video.snippet.title}</h3>
             <p>{video.snippet.description}</p>
-            <iframe
-              width="560"
-              height="315"
-              src={`https://www.youtube.com/embed/${video.id.videoId}`}
-              title={video.snippet.title}
-              frameborder="0"
-              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-              allowfullscreen
-            ></iframe>
-          </li>
+            <div style={{ position: 'relative', paddingBottom: '56.25%', width: '100%', height: '0' }}>
+              <iframe
+                style={{ position: 'absolute', width: '100%', height: '100%' }}
+                src={`https://www.youtube.com/embed/${video.id.videoId}`}
+                title={video.snippet.title}
+                frameBorder="0"
+                allowFullScreen
+              ></iframe>
+            </div>
+          </div>
         ))}
-      </ul>
+      </div>
     </div>
   );
 };
