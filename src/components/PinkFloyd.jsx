d5f23f18b6debd2c1d3b9bac31151428f36bd9b1
dinostagno <dpaolostagno@gmail.com>
2023-12-12 18:14:49 -0300

    update

--- src/components/PinkFloyd.jsx
+++ src/components/PinkFloyd.jsx
@@ -7,31 +7,51 @@ const PinkFloyd = () => {
 
   const videos = [
     // Dark Side of the Moon
-    'JwYX52BP2Sk', // Pink Floyd - "Speak to Me/Breathe"
-    '8UXircX3VdM', // Pink Floyd - "On the Run"
-    'vQf8L1DYlxQ', // Pink Floyd - "Time"
-    'kVYVr3U5s8U', // Pink Floyd - "The Great Gig in the Sky"
-    '0kNGnIKUdMI', // Pink Floyd - "Money"
-    'gP8ZEM5zI0A', // Pink Floyd - "Us and Them"
-    'w1bM3xr1bJ0', // Pink Floyd - "Any Colour You Like"
-    'cVBCE3gaNxc', // Pink Floyd - "Brain Damage"
-    'SLuW-GBaJ8k', // Pink Floyd - "Eclipse"
+    'R49d4f5sEs4', 
+    'JwYX52BP2Sk', 
+    'HoLhKJuGhK0', 
+    'DVQ3-Xe_suY', 
+    'T13se_2A7c8', 
+    'bK7HJvmgFnM', 
+    'DVQ3-Xe_suY', 
+    'cpbbuaIA3Ds', 
+    'G0wOOlwXLgA', 
 
     // Animals
-    'AwNsk5ccA6g', // Pink Floyd - "Pigs on the Wing 1"
-    'YR5ApYxkU-U', // Pink Floyd - "Dogs"
-    'YR5ApYxkU-U', // Pink Floyd - "Pigs (Three Different Ones)"
-    'YR5ApYxkU-U', // Pink Floyd - "Sheep"
-    'YR5ApYxkU-U', // Pink Floyd - "Pigs on the Wing 2"
+    'dd1y-DDpASk', 
+    '4QA30qkRYy8', 
+    'gZM1WQKwpl0', 
+    '3GE-sfEbJ7I', 
+    'kMlLfZJcML0', 
 
     // Echoes
-    '6yCIDkFI7ew', // Pink Floyd - "One of These Days"
-    '6yCIDkFI7ew', // Pink Floyd - "Echoes"
-    '6yCIDkFI7ew', // Pink Floyd - "Astronomy Domine"
-    '6yCIDkFI7ew', // Pink Floyd - "Set the Controls for the Heart of the Sun"
-    '6yCIDkFI7ew', // Pink Floyd - "Careful with That Axe, Eugene"
-    '6yCIDkFI7ew', // Pink Floyd - "One of These Days (Live at Pompeii version)"
-    '6yCIDkFI7ew', // Pink Floyd - "Echoes (Live at Pompeii version)"
+    'T8SEVeC6G2o',
+    'qmOs9ZJ1Vxc',
+    'bBuiptCk518',
+    'E_lu_V15-Co',
+    'Ygb-xprXVyM',
+    'c-MU_5VkjtE',
+    'INPQuOU8hg8',
+    'C8Xc0VptCBY',
+    'bbWOmh_rRq0',
+    'z3cg3IQzSqw',
+    'AsDKoCpG_as',
+    '9xwHFponJN4',
+    '9jPR5n3QlDw',
+    '9pkh8zi_eic',
+    'tfibJ2nhrVo',
+    'yjq3ckItZdE',
+    'QHhNt6q06_k',
+    'ikfPlrVur40',
+    'BgVYyQD05uc',
+    'h90j3lOXNvU',
+    'cRe05aJxSfw',
+    'mybz6bw65y0',
+    'yqdoHPFW_5M',
+    '9I3dgUzBHno',
+    '8HXPLHW1Zno',
+    '6RKjzC7Tg-4',
+    
   ];
 
   const handleVideoReady = () => {
